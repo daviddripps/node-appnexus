f6e7d0c8a45a21aa1af1d51e401ef6ad7362ed28
David Dripps <david.dripps@gmail.com>
2013-06-26 14:09:17 -0700

    fleshed out hte Client.authentication method and added some tests

--- test/client-test.js
+++ test/client-test.js
@@ -5,29 +5,57 @@ var Client = require('../lib/client');
 
 buster.testCase('AppNexus Client', {
   'constructor': {
+    setUp: function() {
+      //stub the authenticate method since we don't actually want to make the API request
+      var testToken = this.testToken = '123456789abcdefghijklmonp';
+
+      this.stub(Client.prototype, 'authenticate').yields(null, testToken);
+
+      this.client = new Client('test_username', 'test_password');
+    },
     'stores the username and password provided': function() {
-      //stub the authenticate() method so we don't actually make the request
-      Client.prototype.authenticate = this.spy();
-
-      var client = new Client('test_username', 'test_password');
-
-      assert.equals(client.username, 'test_username');
-      assert.equals(client.password, 'test_password');
+      assert.equals(this.client.username, 'test_username');
+      assert.equals(this.client.password, 'test_password');
     },
     '//throws if a username or password are not supplied': function() {
-
+      //TODO
     },
     '//merges the provided options over the default options': function() {
       //TODO: create a new Client with no options and store client.options
       //TODO: create a new Client with custom options and compare to the stored options
-    },
-    '//authenticates the user': function() {
-      //TODO: stub the Client.authenticate() method
-      //TODO: call the constructor
-      //TODO: assert that authenticate() was called
     }
   },
   'authenticate()': {
+    setUp: function() {
+      this.client = new Client('test_username', 'test_password');
 
+      //@see https://wiki.appnexus.com/display/api/Authentication+Service - Step 2
+      var responseBody = {
+        "response": {
+          "status": "OK",
+          "token": "h20hbtptiv3vlp1rkm3ve1qig0",
+          "dbg_info": {
+            "instance": "13.hbapi.prod.nym1",
+            "slave_hit": false,
+            "db": "master",
+            "time": 165.15779495239,
+            "start_microtime": 1332339041.1044,
+            "version": "1.11.20"
+          }
+        }
+      };
+
+      this.stub(this.client, 'request').yields(null, null, responseBody);
+    },
+    'returns a session token from the response body': function() {
+      this.client.authenticate(function(err, token) {
+        assert.equals(token, 'h20hbtptiv3vlp1rkm3ve1qig0');
+      });
+    },
+    'assigns the session token to client.token': function() {
+      this.client.authenticate(function(err, token) {
+        assert.equals(this.client.token, token);
+      }.bind(this));
+    }
   }
 });
